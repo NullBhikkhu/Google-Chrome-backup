298703d444dcf2f4577add898083a3d58e375564
Nathan <ARandomScientist@gmail.com>
2017-09-30 18:06:01 -0400

    Improved comment formatting

--- backup-chrome.bash
+++ backup-chrome.bash
@@ -3,21 +3,23 @@
 
 cd ~/
 
-# Check to see if ~/Documents/backups/Google-Chrome/ exists. If it doesn't, create it.
+#  Check to see if ~/Documents/backups/Google-Chrome/ exists. If it doesn't, create it.
 if [ ! -d ~/Documents/backups/Google-Chrome/ ]; then
     echo  "~/Documents/backups/Google-Chrome/ does not exist. Creating it now..."
     mkdir -p ~/Documents/backups/Google-Chrome/
     echo "Done."
 fi
 
-# Check to see if ~/Documents/backups/Google-Chrome/profile.tar.gz exists. If it does, move it to profile.tar.gz.old.
+#  Check to see if ~/Documents/backups/Google-Chrome/profile.tar.gz exists. If it does, move it to profile.tar.gz.old.
 if [ -f ~/Documents/backups/Google-Chrome/profile.tar.gz ]; then
     echo "~/Documents/backups/Google-Chrome/profile.tar.gz already exists. Moving to profile.tar.gz.old..."
     mv ~/Documents/backups/Google-Chrome/profile.tar.gz ~/Documents/backups/Google-Chrome/profile.tar.gz.old
     echo "Done."
 fi
 
-# Check to see if "~/Library/Application Support/Google/Chrome/Default/" exists. If it does, back it up to ~/Documents/backups/Google-Chrome/profile.tar.gz. If it doesn't, notify the user and exit.
+#  Check to see if "~/Library/Application Support/Google/Chrome/Default/" exists.
+#+ If it does, back it up to ~/Documents/backups/Google-Chrome/profile.tar.gz.
+#+ If it doesn't, notify the user and exit.
 if [ -d ~/Library/Application\ Support/Google/Chrome/Default/ ]; then
     echo "Backing up Google Chrome personal files..."
     tar -czf ~/Documents/backups/Google-Chrome/profile.tar.gz  Library/Application\ Support/Google/Chrome/Default/ 2>/dev/null
