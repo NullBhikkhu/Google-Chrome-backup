fd83abb4ffd72e275518e336d3b545049f9f8c83
Nathan <ARandomScientist@gmail.com>
2017-10-05 13:04:16 -0400

    changed backup data directory to ~/.backup-data/Google-Chrome/ from ~/Documents/backups/Google-Chrome/

--- backup-chrome.sh
+++ backup-chrome.sh
@@ -3,29 +3,29 @@
 
 cd ~/
 
-#  Check to see if ~/Documents/backups/Google-Chrome/ exists. If it doesn't, create it.
-if [ ! -d ~/Documents/backups/Google-Chrome/ ]; then
-    echo  "$HOME/Documents/backups/Google-Chrome/ does not exist. Creating it now..."
-    mkdir -p ~/Documents/backups/Google-Chrome/
+#  Check to see if ~/.backup-data/Google-Chrome/ exists. If it doesn't, create it.
+if [ ! -d ~/.backup-data/Google-Chrome/ ]; then
+    echo  "$HOME/.backup-data/Google-Chrome/ does not exist. Creating it now..."
+    mkdir -p ~/.backup-data/Google-Chrome/
     echo "Done."
 fi
 
-#  Check to see if ~/Documents/backups/Google-Chrome/profile.tar.gz exists.
+#  Check to see if ~/.backup-data/Google-Chrome/profile.tar.gz exists.
 #+ If it does, move it to profile.tar.gz.old.
-if [ -f ~/Documents/backups/Google-Chrome/profile.tar.gz ]; then
-    echo "$HOME/Documents/backups/Google-Chrome/profile.tar.gz already exists. Moving to profile.tar.gz.old..."
-    mv ~/Documents/backups/Google-Chrome/profile.tar.gz ~/Documents/backups/Google-Chrome/profile.tar.gz.old
+if [ -f ~/.backup-data/Google-Chrome/profile.tar.gz ]; then
+    echo "$HOME/.backup-data/Google-Chrome/profile.tar.gz already exists. Moving to profile.tar.gz.old..."
+    mv ~/.backup-data/Google-Chrome/profile.tar.gz ~/.backup-data/Google-Chrome/profile.tar.gz.old
     echo "Done."
 fi
 
 #  Check to see if "~/Library/Application Support/Google/Chrome/Default/" exists.
-#+ If it does, back it up to ~/Documents/backups/Google-Chrome/profile.tar.gz.
+#+ If it does, back it up to ~/.backup-data/Google-Chrome/profile.tar.gz.
 #+ If it doesn't, notify the user and exit.
 if [ -d ~/Library/Application\ Support/Google/Chrome/Default/ ]; then
     echo "Backing up Google Chrome personal files..."
-    tar -czf ~/Documents/backups/Google-Chrome/profile.tar.gz  Library/Application\ Support/Google/Chrome/Default/ 2>/dev/null
+    tar -czf ~/.backup-data/Google-Chrome/profile.tar.gz  Library/Application\ Support/Google/Chrome/Default/ 2>/dev/null
     while [ $? -ne 0 ]; do
-        tar -czf ~/Documents/backups/Google-Chrome/profile.tar.gz  Library/Application\ Support/Google/Chrome/Default/ 2>/dev/null
+        tar -czf ~/.backup-data/Google-Chrome/profile.tar.gz  Library/Application\ Support/Google/Chrome/Default/ 2>/dev/null
     done
     echo "Done."
 elif [ ! -d ~/Library/Application\ Support/Google/Chrome/Default/ ]; then
--- restore-backup.sh
+++ restore-backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ###########
 
-backup_file=~/Documents/backups/Google-Chrome/profile.tar.gz
+backup_file=~/.backup-data/Google-Chrome/profile.tar.gz
 orig_dir=~/Library/Application\ Support/Google/Chrome/Default/
 cd ~/
 
-#  If ~/Documents/backups/Google-Chrome/profile.tar.gz exists, remove all the files in
-#+ ~/Library/Application\ Support/Google/Chrome/Default/ and extract ~/Documents/backups/Google-Chrome/profile.tar.gz.
-#+ If ~/Documents/backups/Google-Chrome/profile.tar.gz does not exist, notify the user and exit.
+#  If ~/.backup-data/Google-Chrome/profile.tar.gz exists, remove all the files in
+#+ ~/Library/Application\ Support/Google/Chrome/Default/ and extract ~/.backup-data/Google-Chrome/profile.tar.gz.
+#+ If ~/.backup-data/Google-Chrome/profile.tar.gz does not exist, notify the user and exit.
 if [ -f "$backup_file" ]; then
     echo "Restoring backup..."
     rm -R "$orig_dir" 2>/dev/null
