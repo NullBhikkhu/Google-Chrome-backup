1a3ec2aa102f653c40f04cc5db75a6186a66a106
Nathan <ARandomScientist@gmail.com>
2017-10-05 16:30:30 -0400

    Modified to account for the profile.tar.gz.old file created by backup-chrome.sh.

--- restore-backup.sh
+++ restore-backup.sh
@@ -2,20 +2,32 @@
 ###########
 
 backup_file=~/.backup-data/Google-Chrome/profile.tar.gz
+backup_file_old=~/.backup-data/Google-Chrome/profile.tar.gz.old
 orig_dir=~/Library/Application\ Support/Google/Chrome/Default/
 cd ~/
 
 #  If ~/.backup-data/Google-Chrome/profile.tar.gz exists, remove all the files in
 #+ ~/Library/Application\ Support/Google/Chrome/Default/ and extract ~/.backup-data/Google-Chrome/profile.tar.gz.
-#+ If ~/.backup-data/Google-Chrome/profile.tar.gz does not exist, notify the user and exit.
+#+ If ~/.backup-data/Google-Chrome/profile.tar.gz does not exist,
+#+ check to see if ~/.backup-data/Google-Chrome/profile.tar.gz.old exists.
+#+ If it does, extract it (change to ask if user wants to extract it later),
+#+ if it doesn't, notify the user and exit.
 if [ -f "$backup_file" ]; then
     echo "Restoring backup..."
     rm -R "$orig_dir" 2>/dev/null
     tar -xf "$backup_file"
     echo "Done."
 elif [ ! -f "$backup_file" ]; then
-    echo "No backup file exists. Cannot restore a nonexistant backup: exiting: nothing changed."
-    exit 1
+    if [ -f "$backup_file_old" ]; then
+        echo "profile.tar.gz not found. Restoring from profile.tar.gz.old..."
+        rm -R "$orig_dir" 2>/dev/null
+        tar -xf "$backup_file_old"
+        echo "Done."
+        exit 1
+    elif [ ! -f "$backup_file_old" ]; then
+        echo "No backup files found. Cannot restore backup: exiting."
+        exit 2
+    fi
 fi
 
 
